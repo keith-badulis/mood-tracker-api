5f582eda94b8934aeff71b6f80f743c8858a739b
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2022-08-04 14:18:47 +0800

    added user authorization checks

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -2,98 +2,114 @@ const Entry = require("../models/entry");
 const User = require("../models/user");
 
 exports.entriesGET = async function (req, res, next) {
-  console.log(req.params.username);
+  if (req.session.loggedIn && req.session.username === req.params.username) {
+    const dateFrom = new Date(req.query.dateFrom);
+    const dateTo = new Date(req.query.dateTo);
 
-  const dateFrom = new Date(req.query.dateFrom);
-  const dateTo = new Date(req.query.dateTo);
+    try {
+      const entries = await User.aggregate([
+        { $project: { entries: 1, _id: 0 } },
+        {
+          $lookup: {
+            from: "entries",
+            localField: "entries",
+            foreignField: "_id",
+            as: "entryId",
+          },
+        },
+        { $unwind: "$entryId" },
+        {
+          $project: {
+            emoji: "$entryId.emoji",
+            log: "$entryId.log",
+            date: "$entryId.date",
+          },
+        },
+        {
+          $match: {
+            date: { $gte: dateFrom, $lte: dateTo },
+          },
+        },
+      ]);
 
-  try {
-    const entries = await User.aggregate([
-      { $project: { entries: 1, _id: 0 } },
-      {
-        $lookup: {
-          from: "entries",
-          localField: "entries",
-          foreignField: "_id",
-          as: "entryId",
-        },
-      },
-      { $unwind: "$entryId" },
-      {
-        $project: {
-          emoji: "$entryId.emoji",
-          log: "$entryId.log",
-          date: "$entryId.date",
-        },
-      },
-      {
-        $match: {
-          date: { $gte: dateFrom, $lte: dateTo },
-        },
-      },
-    ]);
-
-    res.send(entries);
-  } catch (error) {
-    console.log(error);
+      res.send(entries);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(401).end();
   }
 };
 
 exports.entryPOST = async function (req, res, next) {
-  try {
-    const newEntry = new Entry({
-      emoji: req.body.emoji,
-      date: new Date(req.body.date),
-      log: req.body.log,
-    });
+  if (req.session.loggedIn && req.session.username === req.params.username) {
+    try {
+      const newEntry = new Entry({
+        emoji: req.body.emoji,
+        date: new Date(req.body.date),
+        log: req.body.log,
+      });
 
-    const entry = await newEntry.save();
-    await User.updateOne(
-      { username: req.params.username },
-      { $push: { entries: entry._id } }
-    );
+      const entry = await newEntry.save();
+      await User.updateOne(
+        { username: req.params.username },
+        { $push: { entries: entry._id } }
+      );
 
-    res.status(201).end();
-  } catch (error) {
-    console.log(error);
-    res.status(500).end();
+      res.status(201).end();
+    } catch (error) {
+      console.log(error);
+      res.status(500).end();
+    }
+  } else {
+    res.status(401).end();
   }
 };
 
 exports.entryGET = async function (req, res, next) {
-  console.log(req.params);
-
-  try {
-    const entry = await Entry.findById(req.params.entryId);
-    res.send(entry);
-  } catch (error) {
-    console.log(error);
+  if (req.session.loggedIn && req.session.username === req.params.username) {
+    try {
+      const entry = await Entry.findById(req.params.entryId);
+      res.send(entry);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(401).end();
   }
 };
 
 exports.entryPUT = async function (req, res, next) {
-  try {
-    const updatedEntry = await Entry.findByIdAndUpdate(req.params.entryId, {
-      emoji: req.body.emoji,
-      date: req.body.date,
-      log: req.body.log,
-    });
-    res.send(updatedEntry);
-  } catch (error) {
-    console.log(error);
+  if (req.session.loggedIn && req.session.username === req.params.username) {
+    try {
+      const updatedEntry = await Entry.findByIdAndUpdate(req.params.entryId, {
+        emoji: req.body.emoji,
+        date: req.body.date,
+        log: req.body.log,
+      });
+      res.send(updatedEntry);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(401).end();
   }
 };
 
 exports.entryDELETE = async function (req, res, next) {
-  try {
-    const deletedEntry = await Entry.findByIdAndDelete(req.params.entryId);
-    await User.updateOne(
-      { username: req.params.username },
-      { $pull: { entries: deletedEntry._id } }
-    );
-    res.status(200).end();
-  } catch (error) {
-    console.log(error);
-    res.status(500).end();
+  if (req.session.loggedIn && req.session.username === req.params.username) {
+    try {
+      const deletedEntry = await Entry.findByIdAndDelete(req.params.entryId);
+      await User.updateOne(
+        { username: req.params.username },
+        { $pull: { entries: deletedEntry._id } }
+      );
+      res.status(200).end();
+    } catch (error) {
+      console.log(error);
+      res.status(500).end();
+    }
+  } else {
+    res.status(401).end();
   }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -14,8 +14,8 @@ exports.userLogin = async function (req, res) {
       req.session.loggedIn = true;
       req.session.username = req.body.username;
       res.send({
-        username: user.username, 
-        nickname: user.nickname
+        username: user.username,
+        nickname: user.nickname,
       });
     } else {
       res.status(401).end();
@@ -28,21 +28,27 @@ exports.userLogin = async function (req, res) {
 
 exports.userPOST = async function (req, res) {
   try {
-    const newUser = new User({
-      username: req.body.username,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      height: req.body.height,
-      weight: req.body.weight,
-      nickname: req.body.nickname,
-      gender: req.body.gender,
-      birthday: new Date(req.body.birthday),
-      entries: [],
-    });
-    newUser.setPassword(req.body.password);
-    await newUser.save();
-
-    res.status(201).end();
+    if (req.session.loggedIn && req.session.username === req.params.username) {
+      const newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        height: req.body.height,
+        weight: req.body.weight,
+        nickname: req.body.nickname,
+        gender: req.body.gender,
+        birthday: new Date(req.body.birthday),
+        entries: [],
+      });
+      
+      newUser.setPassword(req.body.password);
+      
+      await newUser.save();
+      
+      res.status(201).end();
+    } else {
+      res.status(401).end();
+    }
   } catch (error) {
     console.log(error);
     res.status(500).end();
@@ -51,8 +57,12 @@ exports.userPOST = async function (req, res) {
 
 exports.userGET = async function (req, res, next) {
   try {
-    const user = await User.findOne({ username: req.params.username });
-    res.send(user);
+    if (req.session.loggedIn && req.session.username === req.params.username) {
+      const user = await User.findOne({ username: req.params.username });
+      res.send(user);
+    } else {
+      res.status(401).end();
+    }
   } catch (error) {
     console.log(error);
     res.status(500).end();
@@ -61,11 +71,15 @@ exports.userGET = async function (req, res, next) {
 
 exports.userPUT = async function (req, res) {
   try {
-    await User.findOneAndUpdate(
-      { username: req.params.username },
-      { ...req.body }
-    );
-    res.status(200).end();
+    if (req.session.loggedIn && req.session.username === req.params.username) {
+      await User.findOneAndUpdate(
+        { username: req.params.username },
+        { ...req.body }
+      );
+      res.status(200).end();
+    } else {
+      res.status(401).end();
+    }
   } catch (error) {
     console.log(error);
     res.status(500).end();
@@ -74,15 +88,18 @@ exports.userPUT = async function (req, res) {
 
 exports.userDELETE = async function (req, res) {
   try {
-    const deletedUser = await User.findOneAndDelete({
-      username: req.params.username,
-    });
-
-    console.log(deletedUser);
-
-    await Entry.deleteMany({ _id: { $in: deletedUser.entries } });
-
-    res.status(200).end();
+    if (req.session.loggedIn && req.session.username === req.params.username) {
+      // delete user
+      const deletedUser = await User.findOneAndDelete({
+        username: req.params.username,
+      });
+      // delete entries of user
+      await Entry.deleteMany({ _id: { $in: deletedUser.entries } });
+  
+      res.status(200).end();
+    } else {
+      res.status(401).end();
+    }
   } catch (error) {
     console.log(error);
     res.status(500).end();
